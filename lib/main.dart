886e7f86d14e002be7054091cf3798e05077e71b
devheredia <devheredia@gmail.com>
2025-01-08 08:12:13 -0300

    subindo

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,42 @@ class _HomeState extends State<Home> {
         title: Text("Frases do dia"),
         backgroundColor: Colors.green,
       ),
-      body: Container(),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset('images/logo.png'),
+                Text(
+                  "Clique abaixo para gerar uma frase!",
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black,
+                  ),
+                ),
+                ElevatedButton(
+                  child: Text(
+                    "Nova Frase",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight:
+                          FontWeight.bold, // Adicionado o fontWeight como bold
+                      color: Colors.white, // A cor do texto
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green, // Cor de fundo do botão
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        )
     );
   }
 }
